de2955732a377d4ad0f048c3a0fadddbb9d9305f
Tylp <antoineplddev@gmail.com>
2022-12-17 10:24:56 +0100

    add dependenccies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,25 @@
     windows_subsystem = "windows"
 )]
 
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Damn, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("Setting default subscriber failed.");
+
+    info!("Running app");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
